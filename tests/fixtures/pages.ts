1b60da878038013c13291cfa0b1e881aac9972ea
Muneeb Mughal <m.mughal@allspicetech.com.au>
2026-05-17 13:00:19 +0500

    Update Structure

--- tests/fixtures/pages.ts
+++ tests/fixtures/pages.ts
@@ -11,20 +11,13 @@
 
 import { test as base } from '@playwright/test';
 import { ExamplePage } from '../pages/example.page';
-import { GitHubPage } from '../pages/github.page';
 
 export const test = base.extend<{
   examplePage: ExamplePage;
-  githubPage: GitHubPage;
 }>({
   examplePage: async ({ page }, use) => {
     const examplePage = new ExamplePage(page);
     await use(examplePage);
-  },
-
-  githubPage: async ({ page }, use) => {
-    const githubPage = new GitHubPage(page);
-    await use(githubPage);
   },
 });
 

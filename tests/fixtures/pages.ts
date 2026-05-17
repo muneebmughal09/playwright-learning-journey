c29870161c7ea8ccf931c8ddc758dbaabb75f800
Muneeb Mughal <117990061+muneebmughal09@users.noreply.github.com>
2026-05-17 12:33:28 +0500

    Add custom fixtures

---
+++ tests/fixtures/pages.ts
@@ -0,0 +1,24 @@
+/**
+ * 🔧 Page Fixtures
+ */
+
+import { test as base, Page } from '@playwright/test';
+import { ExamplePage } from '../pages/example.page';
+import { GitHubPage } from '../pages/github.page';
+
+export const test = base.extend<{
+  examplePage: ExamplePage;
+  githubPage: GitHubPage;
+}>({
+  examplePage: async ({ page }, use) => {
+    const examplePage = new ExamplePage(page);
+    await use(examplePage);
+  },
+
+  githubPage: async ({ page }, use) => {
+    const githubPage = new GitHubPage(page);
+    await use(githubPage);
+  },
+});
+
+export { expect } from '@playwright/test';
